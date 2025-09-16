3291781a788f12182a44e6b205cedcd14cf1fdb0
gaustin15 <35272713+gaustin15@users.noreply.github.com>
2025-09-16 13:15:06 -0400

    Update 01_load_ptb_data.R

--- PTB/01_load_ptb_data.R
+++ PTB/01_load_ptb_data.R
@@ -9,11 +9,11 @@ data.table::rbindlist( taxa_data$prop$com_V1 %>%
                          lapply(function(x) return(as.data.frame(x$ASV, 
                                                                  row.names=row.names(x))) ), 
                        use.names=TRUE ) %>%
-  write.csv('../../data/cal_ptb/ptb_data.csv')
+  write.csv('ptb_data.csv')
 
 
 
 data.table::rbindlist( lapply( names(taxa_data$prop$com_V1), 
                                function(x) return(taxa_data$meta[[x]] ) ),
                        use.names = T )%>%
-  write.csv('../../data/cal_ptb/ptb_metadata.csv')
+  write.csv('ptb_metadata.csv')
